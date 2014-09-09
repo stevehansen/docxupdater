a6bb700911cbd2a8035c22d28e6218fe035ecbe8
Steve Hansen <steve@2sky.be>
2014-09-09 21:24:09 +0200

    Added v1 code
    * Get .xml and .docx from command line arguments :+1:
    * Show file pickers for missing .xml or .docx :page_facing_up:
    * Find existing XML part and replace :floppy_disk:
    * Embed XML in new executable :punch:

---
+++ DocxUpdater/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace DocxUpdater
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            const string marker = "-=XML=-";
+            const int check = 20;
+
+            var embed = args.Contains("--embed") || args.Contains("/embed");
+            var path = typeof(Program).Assembly.Location;
+            string xmlContents;
+
+            var xmlPath = args.FirstOrDefault(arg => arg.EndsWith(".xml"));
+            if (xmlPath == null)
+            {
+                var contents = File.ReadAllText(path, Encoding.UTF8);
+                var index = contents.IndexOf(marker);
+                if (index == -1)
+                {
+                    var ofd = new OpenFileDialog { Title = "Select XML file", Filter = "XML files|*.xml" };
+                    if (ofd.ShowDialog() == true)
+                        xmlPath = ofd.FileName;
+
+                    if (string.IsNullOrEmpty(xmlPath))
+                        return;
+
+                    xmlContents = File.ReadAllText(xmlPath, Encoding.UTF8);
+                }
+                else
+                    xmlContents = contents.Substring(index + marker.Length + 1); // NOTE: Skip version
+            }
+            else
+                xmlContents = File.ReadAllText(xmlPath, Encoding.UTF8);
+
+            if (embed)
+            {
+                var target = Path.ChangeExtension(path, "." + Path.GetFileNameWithoutExtension(xmlPath) + ".exe");
+                File.Copy(path, target, true);
+                // NOTE: v1 saves XML as cleartext
+                File.AppendAllText(target, marker + '\x01' + xmlContents, Encoding.UTF8);
+                return;
+            }
+
+            var docxPath = args.FirstOrDefault(arg => arg.EndsWith(".docx"));
+            if (docxPath == null)
+            {
+                var ofd = new OpenFileDialog { Title = "Select Microsoft Word document", Filter = "Microsoft Word documents|*.docx;*.dotx" };
+                if (ofd.ShowDialog() == true)
+                    docxPath = ofd.FileName;
+
+                if (string.IsNullOrEmpty(docxPath))
+                    return;
+            }
+
+            // NOTE: Replace XML contents
+            var start = xmlContents.Substring(0, check);
+            using (var package = Package.Open(docxPath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                var parts = package.GetParts().Where(p => p.ContentType == "application/xml" && p.Uri.OriginalString.StartsWith("/customXml")).ToArray();
+                foreach (var xmlPart in parts)
+                {
+                    using (var stream = xmlPart.GetStream(FileMode.Open, FileAccess.ReadWrite))
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var buffer = new char[check];
+                        if (reader.Read(buffer, 0, check) == check && new string(buffer) == start)
+                        {
+                            // NOTE: Found correct part
+                            stream.Position = 0;
+
+                            using (var writer = new StreamWriter(stream))
+                            {
+                                writer.Write(xmlContents);
+                                writer.Flush();
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                package.Flush();
+            }
+        }
+    }
+}
---
+++ DocxUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("DocxUpdater")]
+[assembly: AssemblyDescription("Helper application to update custom XML parts in a Microsoft Word document")]
+[assembly: AssemblyCopyright("Copyright © Steve Hansen 2014")]
+[assembly: AssemblyVersion("1.0")]
